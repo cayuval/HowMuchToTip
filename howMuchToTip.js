de307a2daef97c22d4ed745c189b9cfd6fa6d9a5
cayuval <jubim1234@gmail.com>
2023-03-01 20:36:19 +0200

    minor upgrades

--- howMuchToTip.js
+++ howMuchToTip.js
@@ -4,6 +4,7 @@ let elTipInput = document.getElementById("tipRange")
 let elTipOutput = document.getElementById("howToTip")
 let elBillAmount = document.getElementById("bill")
 let elCurrencySelect = document.getElementById("currency")
+let elTotal = document.getElementById("total")
 
 
 //now - eventListeners
@@ -17,14 +18,21 @@ function calculate(tipPrecentage) {
     let bill = elBillAmount.value
     if (!bill) {
         alert('You must insert bill value')
+    } else {
+
+        let x = tipPrecentage / 100
+        let tip = x * bill
+        if (tip % 1 !== 0) {
+            tip = tip.toFixed(2)
+        }
+        let total = Number(bill) + Number(tip)
+        total = total.toFixed(2)
+        reRender(tip, total)
     }
-    let x = tipPrecentage / 100
-    let tip = x * bill
-    if (tip % 1 !== 0) {
-        tip = tip.toFixed(2)
-    }
-    reRender(tip)
 }
-function reRender(howMuchToTip) {
-    elTipOutput.innerText = `${howMuchToTip} ${elCurrencySelect.value}`
+function reRender(howMuchToTip, total) {
+    const finalTip = Number(howMuchToTip).toLocaleString("en-US")
+    const finalTotal = Number(total).toLocaleString("en-US")
+    elTipOutput.innerText = `${finalTip} ${elCurrencySelect.value}`
+    elTotal.innerText = `Total: ${finalTotal}`
 }
